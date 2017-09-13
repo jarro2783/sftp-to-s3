5d8ce56b601c6cf0a7541ce042d2aed3375a103e
Jarryd Beck <jbeck@freelancer.com>
2017-09-13 15:46:35 +1000

    tidy a little

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const Client = require('ssh2-sftp-client');
+const Client = require('ssh2-sftp-client')
 const streamToString = require('./lib/streamToString')
 const retrieveFileStreams = require('./lib/retrieveFileStreams')
 const uploadToS3 = require('./lib/uploadToS3')
@@ -7,39 +7,37 @@ const SftpToS3 = {
   batch: function(config) {
     const sftp = new Client()
 
-    return new Promise( (resolve, reject) => {
-      return sftp.connect(config.sftp)
-        .then(() => {
-          return sftp.list(config.fileDownloadDir);
+    return sftp.connect(config.sftp)
+      .then(() => {
+        return sftp.list(config.fileDownloadDir)
+      })
+      .then((fileList) => {
+        return retrieveFileStreams(sftp, config, fileList, "sftp")
+      })
+      .then((fileStreams) => {
+        return streamToString(fileStreams)
+      })
+      .then((dataArray) => {
+        return uploadToS3.putBatch(config, dataArray)
+      })
+      .then((files) => {
+        sftp.mkdir(config.completedDir, true)
+        return sftp.list(config.fileDownloadDir)
+      })
+      .then((files) => {
+        files.map((file) => {
+          sftp.rename(config.fileDownloadDir + '/' + file.name,
+            config.completedDir + '/' + file.name)
         })
-        .then((fileList) => {
-          return retrieveFileStreams(sftp, config, fileList, "sftp")
-        })
-        .then((fileStreams) => {
-          return streamToString(fileStreams)
-        })
-        .then((dataArray) => {
-          return uploadToS3.putBatch(config, dataArray)
-        })
-        .then((files) => {
-          sftp.mkdir(config.completedDir, true)
-          return sftp.list(config.fileDownloadDir);
-        })
-        .then((files) => {
-          files.map((file) => {
-            sftp.rename(config.fileDownloadDir + '/' + file.name,
-              config.completedDir + '/' + file.name);
-          })
-          console.log("upload finished")
-          sftp.end()
-          return resolve("ftp files uploaded")
-        })
-        .catch( function(err) {
-          console.error("Error", err);
-          sftp.end()
-          return reject(err)
-        })
-    })
+        console.log("upload finished")
+        sftp.end()
+        return "ftp files uploaded"
+      })
+      .catch( function(err) {
+        console.error("Error", err)
+        sftp.end()
+        throw err
+      })
   }
 }
 
