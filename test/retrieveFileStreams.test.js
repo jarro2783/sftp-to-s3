c22c49240d88d41f3dc22d972b0ca269c95f7e83
Jarryd Beck <jbeck@freelancer.com>
2017-09-14 12:14:08 +1000

    fix file streams path construction

---
+++ test/retrieveFileStreams.test.js
@@ -0,0 +1,42 @@
+'use strict'
+
+const expect = require('chai').expect
+const sinon = require('sinon')
+const streams = require('../lib/retrieveFileStreams')
+
+const sandbox = sinon.createSandbox()
+
+function make_file(name) {
+  return {
+    name: name,
+    type: '-'
+  }
+}
+
+describe('retrieveFileStreams', function() {
+  var sftp = {
+    get: function() {
+    }
+  }
+
+  afterEach(() => {
+    sandbox.restore()
+  })
+
+  it('should call get', function() {
+    var get = sandbox.stub(sftp, 'get').callsFake(function(file) {
+      return Promise.resolve()
+    })
+
+    var config = {
+      fileDownloadDir: 'prefix'
+    }
+
+    return streams(sftp, config, [make_file('foo'), make_file('bar')], 'sftp')
+      .then(() => {
+        sinon.assert.calledTwice(get)
+        sinon.assert.calledWith(get, 'prefix/foo')
+        sinon.assert.calledWith(get, 'prefix/bar')
+      })
+  })
+})
