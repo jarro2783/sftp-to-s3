99489fb72402723e74c1e4a5c2c8901b2b7ddb42
Jarryd Beck <jbeck@freelancer.com>
2017-09-18 16:21:21 +1000

    remove unnecessary semicolons

--- test/index.test.js
+++ test/index.test.js
@@ -1,18 +1,18 @@
-'use strict';
-const expect = require('chai').expect;
+'use strict'
+const expect = require('chai').expect
 const tree = require('../lib/listTree')
-const sinon = require('sinon');
-const SftpToS3 = require('../index');
-const retrieveFileStreams = require('../lib/retrieveFileStreams');
-const Client = require('ssh2-sftp-client');
-const uploadToS3 = require('../lib/uploadToS3');
+const sinon = require('sinon')
+const SftpToS3 = require('../index')
+const retrieveFileStreams = require('../lib/retrieveFileStreams')
+const Client = require('ssh2-sftp-client')
+const uploadToS3 = require('../lib/uploadToS3')
 const config = {
   aws: {
     bucket: 'my-bucket'
   },
   completedDir: 'done',
   fileDownloadDir: 'foo'
-};
+}
 
 const sandbox = sinon.createSandbox()
 
@@ -23,20 +23,20 @@ describe('batch', function() {
 
   it('should run succesfully', function() {
     sandbox.stub(Client.prototype, 'connect').callsFake(function() {
-      return Promise.resolve();
-    });
+      return Promise.resolve()
+    })
 
     sandbox.stub(Client.prototype, 'list').callsFake(function() {
-      return Promise.resolve([{name: 'meow', type: '-'}, {name: 'dir', type: 'd'}]);
-    });
+      return Promise.resolve([{name: 'meow', type: '-'}, {name: 'dir', type: 'd'}])
+    })
 
     sandbox.stub(Client.prototype, 'get').callsFake(function() {
       return Promise.resolve({path: 'foo/meow'})
-    });
+    })
 
     sandbox.stub(Client.prototype, 'mkdir').callsFake(function() {
       return Promise.resolve()
-    });
+    })
 
     sandbox.stub(Client.prototype, 'rename').callsFake(function(from, to) {
       expect(from).to.eq('foo/meow')
@@ -44,43 +44,43 @@ describe('batch', function() {
       return Promise.resolve()
     })
 
-    sandbox.stub(Client.prototype, 'end');
+    sandbox.stub(Client.prototype, 'end')
 
     var s3 = sandbox.stub(uploadToS3, 'putBatch').callsFake(function(config, files) {
       expect(config).to.have.property('fileDownloadDir')
       expect(config).to.have.property('aws')
       expect(files.length).to.equal(1)
       expect(files[0].key).to.equal('foo/meow')
-      return Promise.resolve();
-    });
+      return Promise.resolve()
+    })
 
     return SftpToS3.batch(config)
       .then((success) => {
-        sinon.assert.calledOnce(Client.prototype.connect);
-        sinon.assert.calledOnce(Client.prototype.list);
-        sinon.assert.calledOnce(Client.prototype.mkdir);
-        sinon.assert.calledOnce(Client.prototype.rename);
-        sinon.assert.calledOnce(Client.prototype.end);
+        sinon.assert.calledOnce(Client.prototype.connect)
+        sinon.assert.calledOnce(Client.prototype.list)
+        sinon.assert.calledOnce(Client.prototype.mkdir)
+        sinon.assert.calledOnce(Client.prototype.rename)
+        sinon.assert.calledOnce(Client.prototype.end)
         sinon.assert.calledOnce(s3)
-        expect(success).to.equal('ftp files uploaded');
+        expect(success).to.equal('ftp files uploaded')
       })
-  });
+  })
 
   it('should handle errors', function(done) {
     sandbox.stub(Client.prototype, 'connect').callsFake(function() {
-      return Promise.reject('meowlure');
-    });
+      return Promise.reject('meowlure')
+    })
 
-    sandbox.stub(Client.prototype, 'end');
+    sandbox.stub(Client.prototype, 'end')
 
     SftpToS3.batch(config)
       .catch((err) => {
-        expect(err).to.equal('meowlure');
+        expect(err).to.equal('meowlure')
         done()
       })
-    sinon.assert.calledOnce(Client.prototype.connect);
-  });
-});
+    sinon.assert.calledOnce(Client.prototype.connect)
+  })
+})
 
 describe('recursive', function() {
   afterEach(function() {
@@ -93,8 +93,8 @@ describe('recursive', function() {
     })
 
     sandbox.stub(Client.prototype, 'connect').callsFake(function() {
-      return Promise.resolve({});
-    });
+      return Promise.resolve({})
+    })
 
     sandbox.stub(tree, 'list').callsFake(function() {
       return Promise.resolve([
